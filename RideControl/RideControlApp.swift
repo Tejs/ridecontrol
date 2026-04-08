958c11419f25de7d7db8ac3dcf8c94a547ab894a
Tejs Rasmussen <tejs@creativeforce.io>
2026-04-08 19:42:49 +0200

    Initial Commit

---
+++ RideControl/RideControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  RideControlApp.swift
+//  RideControl
+//
+//  Created by Tejs Rasmussen on 08/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct RideControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
